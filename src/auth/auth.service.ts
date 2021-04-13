99d82a17cb986db92d219cf734cf623cef344661
Victor Campelo <victor_campelo@outlook.com>
2021-04-13 15:46:43 -0300

    feat (AUTH): create authenticate module added controller, entity, roles, service, repository and module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { UserRole } from '../users/user-roles.enum';
+import { User } from '../users/user.entity';
+import { UserRepository } from '../users/users.repository';
+import { CredentialsDto } from './dto/credentials.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {}
+
+  async signUp(createUserDto: CreateUserDto): Promise<User> {
+    if (createUserDto.password != createUserDto.passwordConfirmation) {
+      throw new UnprocessableEntityException('As senhas não conferem');
+    } else {
+      return await this.userRepository.createUser(createUserDto, UserRole.USER);
+    }
+  }
+
+  async signIn(credentialsDto: CredentialsDto) {
+    const user = await this.userRepository.checkCredentials(credentialsDto);
+
+    if (user === null) {
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+  }
+}
