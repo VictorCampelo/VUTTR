78efab95cbaecef7d05c5f015febc775242ef685
Victor Campelo <victor_campelo@outlook.com>
2021-04-13 15:49:43 -0300

    refactor (APP): update modules in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { ToolsModule } from './tools/tools.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), ToolsModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    ToolsModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
