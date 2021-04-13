7330da1a3194e6fbbec8fe4a33373c7b2312cfd0
Victor Campelo <victor_campelo@outlook.com>
2021-04-13 11:41:41 -0300

    feat: created tool repository

---
+++ src/tools/tool.repository.ts
@@ -0,0 +1,30 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { CreateToolDto } from './dtos/create-tool.dto';
+import { Tool } from './tool.entity';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+@EntityRepository(Tool)
+export class ToolRepository extends Repository<Tool> {
+  async createTool(
+    createToolDto: CreateToolDto,
+    //role: UserRole,
+  ): Promise<Tool> {
+    const { title, description, tags } = createToolDto;
+
+    const tool = this.create();
+    tool.title = title;
+    tool.description = description;
+    tool.tags = tags;
+    try {
+      await tool.save();
+      return tool;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Erro ao salvar a ferramenta no banco de dados' + error,
+      );
+    }
+  }
+}
