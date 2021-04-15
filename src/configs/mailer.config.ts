6f724b194ca0c7f0bbfa8a750a5eda7981ba74b2
Victor Campelo <victor_campelo@outlook.com>
2021-04-15 16:54:51 -0300

    feat (CONFIG): config nestjs mailer

---
+++ src/configs/mailer.config.ts
@@ -0,0 +1,28 @@
+import { MailerOptions } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const mailerConfig: MailerOptions = {
+  template: {
+    dir: path.resolve(__dirname, '..', '..', 'templates'),
+    adapter: new HandlebarsAdapter(),
+    options: {
+      extName: '.hbs',
+      layoutsDir: path.resolve(__dirname, '..', '..', 'templates'),
+    },
+  },
+  transport: {
+    host: process.env.HOST_MAIL,
+    port: process.env.PORT_MAIL,
+    auth: {
+      user: process.env.USER_MAIL,
+      pass: process.env.PASS_MAIL,
+    },
+  },
+  defaults: {
+    from: '"nest-modules" <modules@nestjs.com>',
+  },
+};
