7ec1ccffb9430aab4ee34800ba8c08feae5231be
Victor Campelo <victor_campelo@outlook.com>
2021-04-15 16:58:07 -0300

    refactor (CONFIG): add environment to typeorm configurations options

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,13 +1,16 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'mongodb',
-  host: 'localhost',
-  port: 27017,
-  username: 'root',
-  password: 'rootpassword',
-  database: 'test',
+  type: (process.env.DB_TYPE as any) || 'mongodb',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT) || 27017,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || 'rootpassword',
+  database: process.env.DB_BASE || 'test',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  authSource: 'admin',
+  authSource: process.env.DB_AUTH || 'admin',
   synchronize: true,
 };
