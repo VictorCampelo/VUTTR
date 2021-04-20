608d5e52ac6a934994a5944e407c35af809b2203
Victor Campelo <victor_campelo@outlook.com>
2021-04-20 11:41:27 -0300

    style (APP): sort imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './configs/typeorm.config';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { LoggerInterceptor } from './interceptors/logger.interceptor';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { WinstonModule } from 'nest-winston';
-import { winstonConfig } from './configs/winston.config';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { WinstonModule } from 'nest-winston';
+import { AuthModule } from './auth/auth.module';
 import { mailerConfig } from './configs/mailer.config';
+import { typeOrmConfig } from './configs/typeorm.config';
+import { winstonConfig } from './configs/winston.config';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
+import { UsersModule } from './users/users.module';
+
+dotenv.config();
 
 @Module({
   imports: [
